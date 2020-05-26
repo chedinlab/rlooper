9dc447f2778836e2848f5a32b453976b2996841f
Margherita Maria Ferrari <50272676+mmferrari@users.noreply.github.com>
2020-05-25 20:43:08 -0400

    Added option to save graph as PNG

--- graph_rloops3.py
+++ graph_rloops3.py
@@ -7,21 +7,26 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 class RLoopGraph:
     @classmethod
-    def __create_pdf(cls, file_name, probability, energy, start_loci, end_loci):
-        with PdfPages(file_name) as pdf:
-            plt.title('---')
-            plt.subplot(2, 1, 1)
-            plt.ylim(0, 1)
-            plt.xlabel('Position (bp)')
-            plt.ylabel('P(x)')
-            plt.plot(range(int(start_loci), int(end_loci) + 1), probability)
-            plt.subplot(2, 1, 2)
-            plt.plot(range(int(start_loci), int(end_loci) + 1), energy)
-            plt.xlabel('Position (bp)')
-            plt.ylabel('Local Avg G(x)')
-            plt.subplots_adjust(top=0.92, bottom=0.1, left=0.10, right=0.95, hspace=0.25, wspace=1.8)
-            pdf.savefig()
-            plt.close()
+    def __create_file(cls, file_type, file_name, probability, energy, start_loci, end_loci):
+        plt.title(file_name)
+        plt.subplot(2, 1, 1)
+        plt.ylim(0, 1)
+        plt.xlabel('Position (bp)')
+        plt.ylabel('P(x)')
+        plt.plot(range(int(start_loci), int(end_loci) + 1), probability)
+        plt.subplot(2, 1, 2)
+        plt.plot(range(int(start_loci), int(end_loci) + 1), energy)
+        plt.xlabel('Position (bp)')
+        plt.ylabel('Local Avg G(x)')
+        plt.subplots_adjust(top=0.92, bottom=0.1, left=0.10, right=0.95, hspace=0.25, wspace=1.8)
+
+        if file_type == 'pdf':
+            with PdfPages(file_name + '.pdf') as pdf:
+                pdf.savefig()
+        else:
+            plt.savefig(file_name + '.png', dpi=300)
+
+        plt.close()
 
     @classmethod
     def get_args(cls):
@@ -32,10 +37,12 @@ class RLoopGraph:
                             help='Energy WIG file', default=None)
         parser.add_argument('-o', '--output-file-prefix', metavar='OUTPUT_PREFIX', type=str, required=False,
                             help='Output file name prefix', default='')
+        parser.add_argument('-p', '--png', required=False, action='store_const',
+                            help='Output file as PNG image', const='png', default='pdf')
         return parser.parse_args()
 
     @classmethod
-    def plot(cls, probability_file, energy_file, out_filename_prefix=''):
+    def plot(cls, probability_file, energy_file, out_filetype='pdf', out_filename_prefix=''):
         loci_pattern = re.compile(r'\d+-\d+$')
         header1_pattern = re.compile(r'^browser position')
         header2_pattern = re.compile(r'^#')
@@ -54,8 +61,8 @@ class RLoopGraph:
                     if len(probability) > 0:
                         if len(energy) < 1:
                             raise AssertionError('Energy info for ' + seq_name + ' not found')
-                        cls.__create_pdf(out_filename_prefix + seq_name + '.pdf', probability, energy, start_loci,
-                                         end_loci)
+                        cls.__create_file(out_filetype, out_filename_prefix + seq_name, probability, energy,
+                                          start_loci, end_loci)
 
                     probability.clear()
                     energy.clear()
@@ -98,10 +105,11 @@ class RLoopGraph:
         if len(probability) > 0:
             if len(energy) < 1:
                 raise AssertionError('Energy info for ' + seq_name + ' not found')
-            cls.__create_pdf(out_filename_prefix + seq_name + '.pdf', probability, energy, start_loci, end_loci)
+            cls.__create_file(out_filetype, out_filename_prefix + seq_name, probability, energy, start_loci,
+                              end_loci)
 
 
 if __name__ == '__main__':
     args = vars(RLoopGraph.get_args())
     RLoopGraph.plot(args.get('input_file_probability'), args.get('input_file_energy'),
-                    args.get('output_file_prefix', ''))
+                    args.get('png', 'pdf'), args.get('output_file_prefix', ''))
